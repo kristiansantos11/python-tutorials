38d12c8b1ca5f6c099b7aad3994e7287d7c64e50
kristiansantos11 <kristian.santos2000@gmail.com>
2022-08-04 15:04:19 +0800

    Returned student.py back to its original form

--- student.py
+++ student.py
@@ -1,27 +1,20 @@
 class Student:
     major = "CpE"
-    course = "Bachelor of Science Major in "
 
     def __init__(self, stud_id, name):
         self.stud_id = stud_id
         self.name = name
 
-    @staticmethod
-    def get_course():
-        return "CpE"
+print("MAJOR: " + Student.major) # static class attributes can be accessed without instantiating the class itself.
 
-#print("MAJOR: " + Student.major) # static class attributes can be accessed without instantiating the class itself.
-
-# s1 = Student(181, "John")
-# s2 = Student(111, "Bill")
+s1 = Student(181, "John")
+s2 = Student(111, "Bill")
 
 
-# print(s1.major)
-# print(s1.name)
-# print(str(s1.stud_id) + "\n")
-#
-# print(s2.major)
-# print(s2.name)
-# print(str(s2.stud_id) + "\n")
+print(s1.major)
+print(s1.name)
+print(str(s1.stud_id) + "\n")
 
-print(Student.get_course())
+print(s2.major)
+print(s2.name)
+print(str(s2.stud_id) + "\n")
